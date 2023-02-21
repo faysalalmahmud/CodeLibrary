e9b6b867b52f33518dc9b8cd2f44bc21ca274cf3
Faysal Al Mahmud <99035760+faysalalmahmud@users.noreply.github.com>
2023-02-21 23:45:31 +0600

    Create 112A.cpp

---
+++ Codeforces/112A.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string a,b;
+    cin>>a>>b;
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        a[i]=towlower(a[i]);
+        b[i]=towlower(b[i]);
+    }
+
+
+    if (a==b)
+    {
+        cout<<0<<endl;
+    }  
+
+    else{
+
+    for (int i = 0; i < a.size(); i++)
+    {
+
+
+        if (a[i]<b[i])
+        {
+            cout<<-1<<endl;
+            break;
+        }
+
+        else if(a[i]>b[i]) {
+            cout<<1<<endl;
+            break;
+        }
+        
+        
+    }
+}
+
+    return 0;
+}
+
+
