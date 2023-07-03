9389e20c7397e1ec5b0815c9895f2f1b8bdd7361
Faysal Al Mahmud <99035760+faysalalmahmud@users.noreply.github.com>
2023-07-03 11:00:13 +0600

    Create 1002.cpp

---
+++ beecrowd/1002.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    const double pi=3.14159;
+    double R;
+    cin>>R;
+    cout <<fixed<<setprecision(4);
+    cout<<"A="<<pi*R*R<<endl;
+    return 0;
+}
