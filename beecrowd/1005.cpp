ceb0abc4ad90f7c72fd6445ee72cfdfb505ad7e3
Faysal Al Mahmud <99035760+faysalalmahmud@users.noreply.github.com>
2023-07-03 11:04:35 +0600

    Create 1005.cpp

---
+++ beecrowd/1005.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double a,b;
+    cin>>a>>b;
+    cout<<fixed<<setprecision(5)<<"MEDIA = "<<((a*3.5)+(b*7.5))/(3.5+7.5)<<endl;
+    return 0;
+}
