d47c4cb67914b921429ed47c82ebf5d3abe23132
Faysal Al Mahmud <99035760+faysalalmahmud@users.noreply.github.com>
2023-07-16 22:06:37 +0600

    Create F. Coldplay.cpp
    Problem Link : https://algo.codemarshal.org/contests/icpc-dhaka-preli-2017/problems/F

---
+++ ICPC_Dhaka_2018/F. Coldplay.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    long long p , q , y;
+    cin >> p >> q >> y;
+
+    cout << (52*y)*((p*5)+(q*2)) << endl;
+    return 0;
+}
+
+
